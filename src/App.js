5d90c01c1986e56468136973946c34a272e34633
Shirley Suarez <shirley.startary@gmail.com>
2024-04-24 13:08:38 -0600

    feat: add character list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [characters, setCharacters] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+
+	useEffect(() => {
+		fetch("https://rickandmortyapi.com/api/character")
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setCharacters(data.results);
+			});
+	}, []);
+
+	handleOnChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	return (
+		<div className="App">
+			<h1>Rick and Morty</h1>
+			<form>
+				<label htmlFor="Search"></label>
+				<input
+					type="text"
+					id="Search"
+					value={inputValue}
+					onChange={handleOnChange}
+				/>
+				<input type="submit" />
+			</form>
+			<ul>
+				{characters ? (
+					characters.map((character) => {
+						return (
+							<li>
+								<span>{character.name}</span> <span>{character.status}</span>
+							</li>
+						);
+					})
+				) : (
+					<div>Loading...</div>
+				)}
+				<li>
+					<span>name</span> <span>Live</span>
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
